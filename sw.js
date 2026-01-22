92f54c032165b428f6e9b281720695989778788b
Redsguy <andrewmcdonough27@gmail.com>
2026-01-21 21:48:35 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'prayer-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon192.png',
+  './icon512.png',
+  './jesus.png',
+  './mary.png',
+  './morning.png',
+  './day.png',
+  './evening.png',
+  './style.css', 
+  './app.js' 
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
